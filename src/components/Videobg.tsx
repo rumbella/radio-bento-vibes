7dfceef2a95ec2fcd61491b820cf977e7f1cb0c9
rumbella <lapo.ristori@gmail.com>
2025-09-23 13:08:06 +0200

    Update Videobg.tsx

--- src/components/Videobg.tsx
+++ src/components/Videobg.tsx
@@ -35,14 +35,14 @@ const Videobg: React.FC = () => {
         <div className="absolute inset-0 bg-black/30"></div>
       {/* Content Section - DESKTOP */}
       <div className="hidden lg:block lg:w-[65%] p-4">
-        <div className="h-[calc(100vh-12rem)] bg-white/10 backdrop-blur-xl rounded-3xl border border-white/20 shadow-2xl p-6">
+        <div className="h-[calc(100vh-12rem)]  p-6">
           <Content />
         </div>
       </div>
 
       {/* Content Section - MOBILE */}
       <div className="lg:hidden absolute top-4 left-4 right-4 bottom-[220px] p-2">
-        <div className="h-full bg-white/10 backdrop-blur-xl rounded-3xl border border-white/20 shadow-2xl p-6">
+        <div className="h-full  p-6">
           <Content />
         </div>
       </div>
