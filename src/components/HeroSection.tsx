f1f55bf7c03a7edc078ae599ab0a0db544add567
rumbella <lapo.ristori@gmail.com>
2025-09-23 15:38:59 +0200

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -51,7 +51,7 @@ const HeroSection: React.FC<HeroSectionProps> = ({ onSlideChange, slides }) => {
               alt={slide.title}
               className="w-full h-full object-cover"
             />
-            <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/40 to-transparent" />
+            <div className="absolute inset-0" />
           </div>
         ))}
       </div>
