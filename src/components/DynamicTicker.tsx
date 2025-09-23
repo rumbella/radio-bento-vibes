f8ac963bf87269b73a69f664a2bd4549c35bd66f
rumbella <lapo.ristori@gmail.com>
2025-09-23 12:43:12 +0200

    Update DynamicTicker.tsx

--- src/components/DynamicTicker.tsx
+++ src/components/DynamicTicker.tsx
@@ -14,7 +14,7 @@ const DynamicTicker: React.FC<DynamicTickerProps> = ({ slide }) => {
                    slide.type === 'news' ? 'News' : 'Event';
 
   return (
-    <div className="bg-black text-white overflow-hidden w-full">
+    <div className="text-white overflow-hidden w-full">
       <div className="animate-marquee whitespace-nowrap">
         <span className="mx-4 font-bold">{slide.title}</span>
         <span className="mx-4">{slide.subtitle}</span>
