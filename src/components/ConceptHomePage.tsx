8b0c438dbed3072ef29219b4ddc35a314e8bf783
rumbella <lapo.ristori@gmail.com>
2025-09-24 22:43:10 +0200

    Update ConceptHomePage.tsx

--- src/components/ConceptHomePage.tsx
+++ src/components/ConceptHomePage.tsx
@@ -75,7 +75,7 @@ const ConceptHomePage: React.FC = () => {
       </div>
 
       {/* Content Section - MOBILE */}
-      <div className="lg:hidden absolute top-4 left-4 right-4 bottom-[220px] p-2">
+      <div className="lg:hidden absolute top-4 left-4 right-4 bottom-[20px] p-2">
         <div className="h-full p-6">
           <MobileTicker content={mobileContent} />
         </div>
