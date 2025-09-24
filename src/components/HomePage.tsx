ac421cd957b25aa0e15900244607d084dbfba52b
rumbella <lapo.ristori@gmail.com>
2025-09-24 13:08:09 +0200

    Update HomePage.tsx

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -49,7 +49,7 @@ const HomePage: React.FC = () => {
 
       {/* Desktop Layout */}
       <div className="hidden lg:block">
-        <div className="grid h-screen grid-cols-2 grid-rows-[35%_auto] gap-4 px-4 mx-auto lg:flex lg:flex-row lg:h-[calc(100vh-8rem)] lg:px-8 bg-[#000]">
+        <div className="grid h-screen grid-cols-2 grid-rows-[35%_auto] gap-4 px-4 mx-auto lg:flex lg:flex-row lg:h-[calc(100vh-8rem)] lg:px-8 bg-[#151419]">
           {/* Left Sidebar - Top-left on mobile, visible on all screens */}
           <div className="col-start-1 row-start-1 p-4 lg:w-[25%]">
             <div className="h-full p-6 bg-[#1b1b1e] rounded-3xl shadow-2xl lg:h-[calc(100vh-12rem)]">
