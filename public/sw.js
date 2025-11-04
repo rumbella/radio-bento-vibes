bb7b63a150d149160258b399735f5fb70e488d85
rumbella <lapo.ristori@gmail.com>
2025-11-04 13:15:17 +0100

    Update sw.js
    sw modificaro con percorsi corretti icon

--- public/sw.js
+++ public/sw.js
@@ -3,8 +3,8 @@ const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  '/icons/icon-192x192.png',  
+  '/icons/icon-512x512.png'   
 ];
 
 self.addEventListener('install', (event) => {
